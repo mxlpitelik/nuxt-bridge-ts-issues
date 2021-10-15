2abb6d23c95e76b11d6ec4f05f2b3355f0efe90f
Maxim Yudin <mxlpitelik@gmail.com>
2021-10-15 15:42:37 +0300

    typescript trouble

---
+++ app.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+// import {
+//   defineProps,
+//   useRouter,
+//   useStore,
+//   computed,
+//   ref,
+//   onBeforeMount
+// } from '?????';
+
+const props = defineProps({});
+const router = useRouter();
+const store = useStore();
+
+const someComputed = computed(() => "nuxt + ts + composition-api");
+
+onBeforeMount(() => {
+  console.log("works!");
+});
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello Nuxt!</h1>
+    <h2>You are useing {{ someComputed }}</h2>
+  </div>
+</template>
